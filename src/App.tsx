68b7dff6efee29818174b62170a99430a2d913e4
isadorapandas <202869015+isadorapandas@users.noreply.github.com>
2026-07-13 21:49:02 -0300

    Updating urls

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export function App() {
             &copy; {new Date().getFullYear()} World360-AI. All rights reserved.
           </div>
           <div className="footer-credits">
-            Powered by <a href="https://visgraf.impa.br/" target="_blank" rel="noopener noreferrer">VISGRAF Lab</a> &amp; <a href="https://eyllo.com/" target="_blank" rel="noopener noreferrer">Eyllo Tech</a>
+            Powered by <a href="https://visgraf.impa.br/" rel="noopener noreferrer">VISGRAF Lab</a> &amp; <a href="https://eyllo.com/" rel="noopener noreferrer">Eyllo Tech</a>
           </div>
         </div>
       </footer>
